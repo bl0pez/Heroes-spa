efb83ce89ff75bcafdc330bb89102001ea7c91bb
b0pez <blopez4434@gmail.com>
2022-08-30 21:26:12 -0400

    rutas terminadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,18 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
-
+import { HeroesRoutes } from "../heroes";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
-    <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
+      <Routes>
+        
         <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={<HeroesRoutes />} />
 
-        <Route path="*" element={<Navigate to="/marvel" />} />
-
-    </Routes>
-    
+      </Routes>
     </>
-  )
-}
+  );
+};
