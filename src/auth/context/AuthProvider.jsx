14afe6fc53aec23477fbf3b9dfbd9686901a7bcd
b0pez <blopez4434@gmail.com>
2022-09-13 19:48:53 -0300

    configuracion context terminada

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,7 +1,15 @@
+import { useReducer } from 'react';
 import { AuthContext } from './AuthContext';
+import { authReducer } from './AuthReducer';
 
+const initialState = {
+    loading: false,
+}
 
 export const AuthProvider = ({ children }) => {
+
+    const [ state, dispatch ] = useReducer(authReducer, initialState);
+
   return (
     <AuthContext.Provider value={{}}>
         {children}
