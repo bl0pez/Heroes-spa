5fdcd81ee2fe56be61e527b91a5c35df60681744
b0pez <blopez4434@gmail.com>
2022-09-13 21:48:05 -0300

    rutas privadas y publicas

--- src/heroes/layouts/Main.jsx
+++ src/heroes/layouts/Main.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Navbar } from "../../ui";
 
 export const Main = () => {
+
   return (
     <>
       <Navbar />
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,22 +2,42 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import { DcPage, HeroPage, Main, MarvelPage, SearchPage } from "../heroes";
 import { LoginPage } from "../auth/pages/LoginPage";
+import { useContext } from "react";
+import { AuthContext } from "../auth";
 
 export const AppRouter = () => {
+
+  const { logged } = useContext(AuthContext);
+
   return (
     <>
       <Routes>
-        
-        <Route path="/login" element={<LoginPage />} />
 
-        <Route path="/" element={<Main />} >
-          <Route path="dc" element={<DcPage />} />
-          <Route path="marvel" element={<MarvelPage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route path="hero/:id" element={<HeroPage />} />
-        </Route>
+        {
+          logged
+            ?
+            (
+              <Route path="*" element={<Main />} >
+                <Route path="dc" element={<DcPage />} />
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="search" element={<SearchPage />} />
+                <Route path="hero/:id" element={<HeroPage />} />
+                <Route path="*" element={<Navigate to="/marvel" />} />
+              </Route>
 
-        <Route path="*" element={<Navigate to="/login" />} />
+            )
+
+            :
+            (
+
+              <>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+              </>
+
+            )
+
+        }
 
       </Routes>
     </>
