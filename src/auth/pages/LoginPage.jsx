5a2cd2d1370ca2cc2966973f1cbf71231678b3f5
b0pez <blopez4434@gmail.com>
2023-01-28 22:47:16 -0300

    nombre dinamico

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context';
 
@@ -6,11 +6,17 @@ export const LoginPage = () => {
 
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
+  //State para capturar el nombre del usuario
+  const [user, setUser] = useState('');
 
   const handleLogin = () => {
 
+    if(user.trim().length < 2) return;
+
     const lastPath = localStorage.getItem('lastPath') || '/';
-    login('Nombre de Prueba');
+
+
+    login(user);
 
     navigate(lastPath, {
       replace: true,
@@ -21,6 +27,8 @@ export const LoginPage = () => {
     <div className='content-center'>
       <div className='text-center d-grid gap-2 col-2 mx-auto'>
         <h1>Login</h1>
+
+        <input type='text' placeholder='Nombre de usuario' className='form-control' value={user} onChange={(e) => setUser(e.target.value)} />
 
         <button
           className='btn btn-primary'
